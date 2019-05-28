7461fe9db7d0d7ee040fe9601d434806e51ddcd7
k3yro <zeilenfang@gmail.com>
2019-05-28 19:34:45 +0200

    added LibGTX project data

---
+++ k3-gdx-game/android/src/com/k3yro/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.k3yro.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.k3yro.game.K3GdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new K3GdxGame(), config);
+	}
+}
---
+++ k3-gdx-game/desktop/src/com/k3yro/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.k3yro.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.k3yro.game.K3GdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new K3GdxGame(), config);
+	}
+}
