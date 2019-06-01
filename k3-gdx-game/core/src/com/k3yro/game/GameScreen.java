3b19763ed019b131412904162780f0b82b8623a4
k3yro <zeilenfang@gmail.com>
2019-06-01 20:51:21 +0200

    TextureAtlas vs Sprite

--- k3-gdx-game/core/src/com/k3yro/game/GameScreen.java
+++ k3-gdx-game/core/src/com/k3yro/game/GameScreen.java
@@ -1,19 +1,46 @@
 package com.k3yro.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class GameScreen extends ScreenAdapter {
 
-
+    private Texture texture;
+    //private TextureRegion planetTexture;
+    //private TextureAtlas atlas;
+    private SpriteBatch batch;
+    private Sprite sprite;
+    int direction = 1;
 
     public GameScreen() {
-
+        //atlas = new TextureAtlas("planets.atlas");
+        texture = new Texture("badlogic.jpg");
+        batch = new SpriteBatch();
+        sprite = new Sprite(texture);
     }
 
     @Override
     public void render(float delta) {
-
-
+        Gdx.gl.glClearColor(0,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        //batch.draw(texture, 0.0f, 0.0f, texture.getWidth(), texture.getHeight());
+        sprite.draw(batch);
+        sprite.rotate(delta * 90.0f);
+        if (sprite.getX() >= Gdx.graphics.getWidth()){
+            direction = -1;
+        } else if (sprite.getX() <= 0){
+            direction = 1;
+        }
+        sprite.translate(delta * 50 * direction, 50*delta*direction);
+        System.out.println(sprite.getX());
+        batch.end();
     }
 
     @Override
--- k3-gdx-game/desktop/src/com/k3yro/game/desktop/DesktopLauncher.java
+++ k3-gdx-game/desktop/src/com/k3yro/game/desktop/DesktopLauncher.java
@@ -3,10 +3,21 @@ package com.k3yro.game.desktop;
 import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 import com.k3yro.game.K3GdxGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+		TexturePacker.Settings settings = new TexturePacker.Settings();
+		settings.maxHeight = 4096;
+		settings.maxWidth = 4096;
+		settings.edgePadding = true;
+		settings.duplicatePadding = true;
+		settings.filterMin = Texture.TextureFilter.Linear;
+		settings.filterMag = Texture.TextureFilter.Linear;
+		TexturePacker.process("Planeten", ".", "planets");
+
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.useVsync(true);
 		//config.fullscreen = true;
