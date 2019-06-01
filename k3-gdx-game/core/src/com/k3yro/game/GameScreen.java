9c367a7ba2eae379d6562131086a2bc229ebf8f4
k3yro <zeilenfang@gmail.com>
2019-06-01 18:09:23 +0200

    ping pong

--- k3-gdx-game/core/src/com/k3yro/game/GameScreen.java
+++ k3-gdx-game/core/src/com/k3yro/game/GameScreen.java
@@ -14,25 +14,30 @@ public class GameScreen extends ScreenAdapter {
 
     ShapeRenderer shapeRenderer;
     Vector2 paddlePosition;
+    Vector2 enemyPosition;
     Vector2 ballPosition;
     Vector2 ballVelocity;
 
     float maxVelocity = 400.0f;
-    float paddleWidth = 60.0f;
-    float paddleHeight = 15.0f;
-    float ballRadius = 10.0f;
-    float paddleOffset = 30.0f;
+    float startSpeed = 600.0f;
+    float paddleWidth = 140.0f;
+    float paddleHeight = 28.0f;
+    float ballRadius = 23.0f;
+    float paddleOffset = 50.0f;
     Color paintColor = Color.BLACK;
     float bgColor = 1;
     Random random = new Random();
 
+    float enemyOffset = Gdx.graphics.getHeight() - paddleOffset - paddleHeight;
+
     public GameScreen() {
         shapeRenderer = new ShapeRenderer();
         paddlePosition = new Vector2(Gdx.graphics.getWidth() / 2.0f - paddleWidth / 2.0f, paddleOffset);
+        enemyPosition = new Vector2(Gdx.graphics.getWidth() / 2.0f - paddleWidth / 2.0f, enemyOffset);
         ballPosition = new Vector2(Gdx.graphics.getWidth() / 2.0f, Gdx.graphics.getHeight() / 2.0f - ballRadius);
 
         // nextFloat -> Zufallszahl zwischen 0 und 1
-        ballVelocity = new Vector2((random.nextFloat() * 200) - 100, 100.0f);
+        ballVelocity = new Vector2((random.nextFloat() * 200) - 100, startSpeed);
     }
 
     @Override
@@ -43,6 +48,16 @@ public class GameScreen extends ScreenAdapter {
         if (Gdx.input.isKeyPressed(Input.Keys.D)){
             paddlePosition.x +=delta * maxVelocity;
         }
+
+        if (paddlePosition.x <= (Gdx.graphics.getWidth()-100.0f) && Gdx.input.getAccelerometerX() < 0)
+        {
+            paddlePosition.x -= Gdx.input.getAccelerometerX() * 5;
+        }
+        else if (paddlePosition.x >= 0 && Gdx.input.getAccelerometerX() > 0)
+        {
+            paddlePosition.x -= Gdx.input.getAccelerometerX() * 5;
+        }
+
         if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)){
             Gdx.app.exit();
         }
@@ -52,12 +67,6 @@ public class GameScreen extends ScreenAdapter {
             paddlePosition.x = 0.0f;
         }else if (paddlePosition.x > Gdx.graphics.getWidth() - paddleWidth ){
             paddlePosition.x = Gdx.graphics.getWidth() - paddleWidth;
-        }
-
-        // Ballreflektion oben
-        if (ballPosition.y >= Gdx.graphics.getHeight() - ballRadius){
-            ballPosition.y = Gdx.graphics.getHeight() - ballRadius;
-            ballVelocity.y *= -1;
         }
 
         // Ballreflektion rechts
@@ -76,16 +85,45 @@ public class GameScreen extends ScreenAdapter {
         if (ballPosition.y <= ballRadius){
             paddlePosition = new Vector2(Gdx.graphics.getWidth() / 2.0f - paddleWidth / 2.0f, paddleOffset);
             ballPosition = new Vector2(Gdx.graphics.getWidth() / 2.0f, Gdx.graphics.getHeight() / 2.0f - ballRadius);
-            ballVelocity = new Vector2((random.nextFloat() * 200) - 100, 100.0f);
+            ballVelocity = new Vector2((random.nextFloat() * 200) - 100, startSpeed);
+        }
+
+        // Win
+        if (ballPosition.y >= Gdx.graphics.getHeight() + ballRadius){
+            paddlePosition = new Vector2(Gdx.graphics.getWidth() / 2.0f - paddleWidth / 2.0f, paddleOffset);
+            ballPosition = new Vector2(Gdx.graphics.getWidth() / 2.0f, Gdx.graphics.getHeight() / 2.0f - ballRadius);
+            ballVelocity = new Vector2((random.nextFloat() * 200) - 100, startSpeed);
+        }
+
+        // KI
+        if (ballPosition.x >= enemyPosition.x + paddleWidth - paddleWidth / 3.0f){
+            enemyPosition.x += 5.3f;
+        }else if(ballPosition.x <= enemyPosition.x + paddleWidth / 3.0f) {
+            enemyPosition.x -= 5.3f;
+        }
+
+        // Ballreflektion Enemy
+        if (ballPosition.y + ballRadius >= enemyPosition.y){
+            if (ballPosition.x >= enemyPosition.x && ballPosition.x <= enemyPosition.x + paddleWidth){
+
+
+                if (ballVelocity.y > 0)
+                    ballVelocity.y *= -1.0;
+
+                System.out.println(ballVelocity.y);
+
+
+            }
+
         }
 
         // Ballreflektion Paddle
-        if (ballPosition.y - ballRadius <= paddlePosition.y + paddleHeight && ballPosition.y + ballRadius > paddlePosition.y){
+        if (ballPosition.y - ballRadius <= paddlePosition.y + paddleHeight ){
             if (ballPosition.x >= paddlePosition.x && ballPosition.x <= paddlePosition.x + paddleWidth){
                 ballPosition.y = paddlePosition.y + paddleHeight + ballRadius;
-                if (ballVelocity.y >= -400.0f) {
+                if (ballVelocity.y >= -1400.0f) {
                     ballVelocity.y *= -1.4;
-                    System.out.println("sdfjsl");
+                   // System.out.println("sdfjsl");
                 }
                 else {
                     ballVelocity.y *= -0.8;
@@ -99,10 +137,9 @@ public class GameScreen extends ScreenAdapter {
                         bgColor = 1;
                     }
                 }
-                ballVelocity.x = random.nextFloat() * 200 - 100;
 
+                ballVelocity.x = (random.nextFloat() * 1200) - 600;
             }
-
         }
 
         // Ball Geschwindigkeit berechnen
@@ -114,6 +151,7 @@ public class GameScreen extends ScreenAdapter {
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
         shapeRenderer.setColor(paintColor);
         shapeRenderer.rect(paddlePosition.x, paddlePosition.y, paddleWidth, paddleHeight);
+        shapeRenderer.rect(enemyPosition.x, enemyPosition.y, paddleWidth, paddleHeight);
         shapeRenderer.circle(ballPosition.x, ballPosition.y, ballRadius);
         shapeRenderer.end();
     }
